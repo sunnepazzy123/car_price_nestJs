25e9ba5d3fb0f151b85829b26ab075db86ee4503
sunday odibo <sunnepazzy123@gmail.com>
2022-08-30 15:50:38 +0200

    setup heroku

--- dist/reports/reports.entity.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class Reports {
-    id: number;
-    price: number;
-}
--- dist/users/users.entity.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class User {
-    id: number;
-    email: string;
-    password: string;
-    beforeInsert(): void;
-    logInsert(): void;
-    logUpdate(): void;
-    logDelete(): void;
-}
